49a83325d5a891b40a56b868865997ffb864aafc
piveT0 <joaos.mae132@gmail.com>
2024-06-20 06:23:06 -0400

    feat: month, week, hours, minutes and seconds

--- src/components/progressBar.tsx
+++ src/components/progressBar.tsx
@@ -2,12 +2,19 @@
 
 import * as React from 'react';
 import { Progress } from '@/components/ui/progress';
-import { percentageOfYearPassed } from '@/helpers/yearPercent';
+import {
+	daysPassedInYear,
+	timesPassedSinceStartOfYear,
+	percentageOfYearPassed,
+	weekAndMonthPassed,
+} from '@/helpers/yearPercent';
 
 export function ProgressDemo() {
 	const today: Date = new Date();
 
 	const percentProgress = percentageOfYearPassed(today);
+	const timesPassed = timesPassedSinceStartOfYear(today);
+	const monthAndWeekPassed = weekAndMonthPassed(today);
 
 	const [progress, setProgress] = React.useState<number>(0);
 
@@ -17,8 +24,21 @@ export function ProgressDemo() {
 	}, [percentProgress]);
 
 	return (
-		<div className="flex flex-col justify-center items-center gap-2">
-			<h1 className="text-2xl">Porcentagem do ano: {percentProgress.toFixed(2)}%</h1>
+		<div className="flex flex-col justify-center items-center gap-2 ">
+			<span className='self-start w-full'>
+				<h1 className="text-2xl text-[#16A34A] font-semibold text-center">
+					Porcentagem do ano: {percentProgress.toFixed(2)}%
+				</h1>
+				<h1 className="text-2xl text-[#ccc]">Horas passadas: {timesPassed.diffInHours}</h1>
+				<h1 className="text-2xl text-[#ccc]">Minutos passados: {timesPassed.minutes}</h1>
+				<h1 className="text-2xl text-[#ccc]">Segundos passados: {timesPassed.seconds}</h1>
+				<h1 className="text-2xl text-[#ccc]">
+					Semanas passadas: {monthAndWeekPassed.weekPassed.toFixed(2)}
+				</h1>
+				<h1 className="text-2xl text-[#ccc]">
+					Meses passados: {monthAndWeekPassed.monthPassed.toFixed(2)}
+				</h1>
+			</span>
 
 			<Progress value={progress} className="w-96 h-8" />
 		</div>
--- src/helpers/yearPercent.ts
+++ src/helpers/yearPercent.ts
@@ -22,6 +22,27 @@ export function daysPassedInYear(date: Date): number {
 	return daysPassed;
 }
 
+export function timesPassedSinceStartOfYear(date: Date): {
+	diffInHours: number;
+	minutes: number;
+	seconds: number;
+} {
+	const daysPassed = daysPassedInYear(date);
+
+	const diffInHours: number = daysPassed * 24;
+	const minutes: number = daysPassed * 60;
+	const seconds: number = daysPassed * 24 * 60 * 60;
+	return { diffInHours, minutes, seconds };
+}
+
+export function weekAndMonthPassed(date: Date): { weekPassed: number; monthPassed: number } {
+	const daysPassed = daysPassedInYear(date);
+	const weekPassed = daysPassed / 7;
+	const monthPassed = daysPassed / 30.44;
+
+	return { weekPassed, monthPassed };
+}
+
 export function percentageOfYearPassed(date: Date): number {
 	const year = date.getFullYear();
 	const totalDays = isLeapYear(year) ? 366 : 365;
